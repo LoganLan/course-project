1caab6ac2ad812210b1752a6a008e25fbcc608de
Logan Lan <loganlan2001@gmail.com>
2021-06-17 15:29:49 -0700

    I added a new recipe model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './root/header/header.component';
+import { RecipeBookComponent } from './root/Header/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './root/Header/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './root/Header/recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeItemDetailsComponent } from './root/header/recipe-book/recipe-list/recipe-item-details/recipe-item-details.component';
+import { ShoppingListComponent } from './root/Header/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './root/Header/shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeItemDetailsComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
